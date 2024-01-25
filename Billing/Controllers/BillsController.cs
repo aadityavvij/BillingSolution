1d6a180cb492dcaa35726275a58b5cf7bda0d897
Aaditya <aadityavardhanvij.bt22cse@pec.edu.in>
2024-01-25 23:29:40 +0530

    Add project files.

---
+++ Billing/Controllers/BillsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Billing.Controllers
+{
+    public class BillsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
